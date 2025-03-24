1338bafc5c712e45c441779a68c2845a46d3005d
gitvuyelwa <vuyi.macanda@gmail.com>
2025-03-24 22:34:55 +0200

    update

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -4,3 +4,5 @@ var lastModified = document.lastModified;
 
 console.log("Last modified date: " + lastModified);
 
+calculateWindChill(50, 10);
+windchill = 35.74 + 0.6215 * temp + (0.4275 * temp - 35.75) * Math.pow(speed, 0.16);
