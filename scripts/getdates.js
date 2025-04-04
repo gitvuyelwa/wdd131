6fff60d7d37239976c24727bf3a4924c865465b9
gitvuyelwa <vmacanda@byupathway.edu>
2025-04-04 10:00:27 +0200

    update

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,8 +1,8 @@
+function lastModified(){
+    const lastModified = document.lastModified;
+    document.getElementById("lastModif").textContent = lastModified;
+}
 
-let oLastModif = new Date(document.lastModified);
-var lastModified = document.lastModified;
-
-console.log("Last modified date: " + lastModified);
-
-calculateWindChill(50, 10);
-windchill = 35.74 + 0.6215 * temp + (0.4275 * temp - 35.75) * Math.pow(speed, 0.16);
+function calculateWindChill(speed, temp) {
+    let windchill = 35.74 + 0.6215 + temp - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16);
+}
